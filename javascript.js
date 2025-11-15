a959621593300da7557c59cd699c3f5091f939d2
Julian <111262742+Marentxs@users.noreply.github.com>
2025-11-15 10:58:58 -0500

    completed function for gameboard

--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
 const gameboard = (function () {
   const rows = 3;
   const columns = 3;
-  let board = [];
+  const board = [];
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       board[i].push(Cell());
     }
   }
+
+  const getBoard = () => board;
+
+  const dropToken = (column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+
+    if (!availableCells.length) return;
+
+    board[row][column].addToken(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, dropToken, printBoard };
 })();
 
 const flow = (function () {
